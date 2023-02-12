e4b05cd8b5c3d396d0967c526f54817b17b50c1c
itbergl <tberg644@gmail.com>
2023-02-12 12:10:17 +0800

    versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ import setuptools
 
 
 setuptools.setup(name='qfinuwa',
-      version='1.0.0',
       description='Framework for backtesting quantitative trading algorithims.',
       package_dir = {"": "src"},
       packages = setuptools.find_packages(where="src"),
